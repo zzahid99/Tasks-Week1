aa217685a27aa1f1d40d9bfea4077387d1dba887
Zamar Zahid <zzahid@nisum.com>
2021-06-10 12:35:24 +0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        // Task 1
+//        int[] arr = {3,4,7,8,32,54,67,77};
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2 == 0) {
+//                System.out.println(arr[i] + " is an even number");
+//            } else {
+//                System.out.println(arr[i] + " is a odd number");
+//            }
+//        }
+
+        // Task 2
+//        int[] arr = {3,4,7,8,32,99,54,67,77};
+//        int temp = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (temp < arr[i]) {
+//                temp = arr[i];
+//            }
+//        }
+//        System.out.println("The maximum number in the list is "+ temp);
+
+        // Task 3
+//        int[] arr = {3,4,7,8,32,99,54,67,77,1};
+//        int temp = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (temp > arr[i]) {
+//                temp = arr[i];
+//            }
+//        }
+//        System.out.println("The minimum number in the list is "+ temp);
+
+        // Task 4 and Task 5
+//        int[] arr = {3,4,5,6,7};
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum += arr[i];
+//        }
+//        System.out.println("The sum is " + sum);
+//        System.out.println("The average is " + sum/arr.length);
+
+        // Task 6
+//        int[] arr = {3,4,7,8,32,99,54,67,77,1};
+//        int n = 9;
+//        boolean flag = false;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == n) {
+//                flag = true;
+//            }
+//        }
+//        if (flag) {
+//            System.out.println("Found!!");
+//        } else {
+//            System.out.println("Not Found");
+//        }
+    }
+}
